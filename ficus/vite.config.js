f59edb7fddfc3554b29bb98334cdb450fe3a8a89
Francesco Zerbinato <francesco.zerbinato@gmail.com>
2026-02-12 14:49:24 +0100

    resolving conflict vite

--- ficus/vite.config.js
+++ ficus/vite.config.js
@@ -7,6 +7,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  json: {
+    stringify: false  // 👈 AGGIUNGI QUESTA RIGA
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
